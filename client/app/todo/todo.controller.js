7da5e27f5c65f6f82a886dbac9a3817834f8ad68
Rubens Mariuzzo <rubens@mariuzzo.com>
2014-07-30 01:20:57 -0400

    Styling and removing unused bits of code.

--- client/app/todo/todo.controller.js
+++ client/app/todo/todo.controller.js
@@ -1,10 +1,9 @@
 'use strict';
 
 angular.module('todoApp')
-  .controller('TodoCtrl', function ($scope, $http) {
-    $scope.status = 'Loading...';
-    $http.get('/api/todos').success(function(data) {
-        $scope.status = 'Done...';
-        $scope.todos = data;
+    .controller('TodoCtrl', function($scope, $http) {
+        // Get todos.
+        $http.get('/api/todos').success(function(data) {
+            $scope.todos = data;
+        });
     });
-  });
